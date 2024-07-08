cb9e918ac4fd43da851240cfe3db9ac6e6d7d077
w4-h1d <86832557+w4-h1d@users.noreply.github.com>
2024-07-08 10:24:46 +0400

    menu button fix

--- scripts.js
+++ scripts.js
@@ -1,4 +1,26 @@
-const year = document.getElementById("year")
-const thisYear = new Date().getFullYear()
-year.setAttribute("datetime", thisYear)
-year.textContent = thisYear
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
+
+document.addEventListener('DOMContentLoaded', function () {
+    let menu = document.querySelector('.menu-button');
+    let cmenu = document.querySelector('.close-menu-button');
+    let nav = document.querySelector('.header__nav');
+
+    menu.onclick = function () {
+        nav.style.cssText = 
+        'display: block; animation: showMenu 0.5s ease-in-out forwards';
+        menu.style.display = 'none';
+        cmenu.style.display = 'block';
+        console.log('nav menu opened');
+    };
+
+    cmenu.onclick = function() {
+        menu.style.display = 'block';
+        cmenu.style.display = 'none';
+        nav.style.cssText = 
+        'display: block; animation: hideMenu 0.5s ease-in-out forwards';
+        console.log('nav menu closed'); 
+    };
+});
